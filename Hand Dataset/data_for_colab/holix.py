ed0f3fc619d3fda7e7b3e5c6e083bc430404fddd
MrGonza <38736930+Dezgonza@users.noreply.github.com>
2019-11-15 22:17:22 -0300

    add

--- Hand Dataset/data_for_colab/holix.py
+++ Hand Dataset/data_for_colab/holix.py
@@ -1,5 +1,5 @@
-file =  open('train.txt', 'r')
-file1 =  open('train1.txt', 'w')
-k = '/content/darknet/data_for_colab/data/'
+file =  open('test.txt', 'r')
+file1 =  open('test1.txt', 'w')
+k = '/content/Colab/HandDataset/data_for_colab/data'
 for i in file:
-    file1.write(k + i[i.index('/')+1:])
+    file1.write(k + i[i.index('data/')+4:])
